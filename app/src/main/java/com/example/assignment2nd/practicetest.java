00c4725e53326837d696bd0bed17aaa3f0bb448a
waqarjutt351 <63953723+waqarjutt351@users.noreply.github.com>
2021-03-07 10:50:05 +0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment2nd/practicetest.java
@@ -0,0 +1,14 @@
+package com.example.assignment2nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class practicetest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practicetest);
+    }
+}
